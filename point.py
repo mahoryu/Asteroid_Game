211658aba4cefe49e2b5cf5dce0dbb1d9b451693
Ethan Holden <mahoryu@gmail.com>
2020-12-09 21:22:04 -0700

    Added a __str__ method to Point

--- point.py
+++ point.py
@@ -5,11 +5,18 @@ class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f"({x},{y})"
+
     def get_x(self):
         return self.x
+
     def get_y(self):
         return self.y
+
     def set_x(self, x):
         self.x = x
+
     def set_y(self, y):
         self.y = y
