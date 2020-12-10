0dde8230958ae3514f62f1d88e1b0754cf0b442e
Ethan Holden <mahoryu@gmail.com>
2020-12-09 21:17:20 -0700

    Added a point classfor use in other classes

---
+++ point.py
@@ -0,0 +1,16 @@
+class Point():
+    """
+    A point object containing an x and y coordinate
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return x
+    def get_y(self):
+        return y
+    def set_x(self, x):
+        self.x = x
+    def set_y(self, y):
+        self.y = y
