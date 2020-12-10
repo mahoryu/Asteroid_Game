ec9140cc4003cbc545a52a784e09df2bf0fefd16
Ethan Holden <mahoryu@gmail.com>
2020-12-09 22:05:22 -0700

    set up empty methods that will be used for the FlyingObject class.

---
+++ flyingObject.py
@@ -0,0 +1,18 @@
+import point
+import velocity
+
+class FlyingObject():
+    """
+    The base class for any object that will fly accross the game screen.
+    """
+    def __init__(self):
+        pass
+
+    def setAngle(self):
+        pass
+
+    def wrap(self):
+        pass
+
+    def advance(self):
+        pass
